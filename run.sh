f41a175b50bea4746482bab5823258e3a168fff4
Tim 'mithro' Ansell <me@mith.ro>
2019-11-04 09:49:20 -0700

    Output hostinfo + hostsetup.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $SCRIPT_DIR/travis.sh
 
 set -x
 set -e
 
+# Set up the host
+source $SCRIPT_DIR/steps/hostsetup.sh
+source $SCRIPT_DIR/steps/hostinfo.sh
+
+# Output git information
 ls -l
 cd github
 ls -l
 cd vtr-verilog-to-routing-continuous
+source $SCRIPT_DIR/steps/git.sh
 
-echo "--------------------------------------"
-git status
-echo "--------------------------------------"
-git log -n1
-echo "--------------------------------------"
-
+# Travis compatibility script
+source $SCRIPT_DIR/travis.sh
 ./.github/travis/build.sh
 ./run_reg_test.pl $VTR_TEST -show_failures -j$NUM_CORES
