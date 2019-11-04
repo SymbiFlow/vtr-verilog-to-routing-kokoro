5cdbd0aa4f0b95645bec005bd67b32be2bb9254b
Tim 'mithro' Ansell <me@mith.ro>
2019-11-04 12:12:15 -0700

    Trying some paths.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- steps/vtr-test.sh
+++ steps/vtr-test.sh
@@ -10,4 +10,13 @@ if [ -z $MAX_CORES ]; then
 	exit 1
 fi
 
+echo $PWD
+pwd
+pwd -L
+pwd -P
+cd $(realpath $(pwd))
+echo $PWD
+pwd
+pwd -L
+pwd -P
 ./run_reg_test.pl $VTR_TEST -show_failures -j$MAX_CORES
